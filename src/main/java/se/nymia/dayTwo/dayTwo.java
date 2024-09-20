add37abc1fc928e81249446f893280396eeada08
Emma Mccall <xNymia@users.noreply.github.com>
2024-09-20 22:20:37 +0200

    learned about XOR

--- src/main/java/se/nymia/dayTwo/dayTwo.java
+++ src/main/java/se/nymia/dayTwo/dayTwo.java
@@ -18,39 +18,45 @@ public class dayTwo {
             return;
         }
 
-        // --------- VERSION 1 ----------
-//        for (String s : input) {
-//            List<String> str = Arrays.asList(s.split(" "));
-//
-//            char value = str.get(1).charAt(0);
-//            String[] temp = str.get(0).split("-");
-//            int min = Integer.parseInt(temp[0]);
-//            int max = Integer.parseInt(temp[1]);
-//            String pass = str.get(2).trim();
-//
-//            int count = (int) pass.chars().filter(c -> c == value).count();
-//
-//            if (count >= min && count <= max) {
-//                valid++;
-//            }
-//        }
-//        System.out.println(valid);
 
+        /* --------- VERSION 1 ----------
+        for (String s : input) {
+            List<String> str = Arrays.asList(s.split(" "));
+
+            char value = str.get(1).charAt(0);
+            String[] temp = str.get(0).split("-");
+            int min = Integer.parseInt(temp[0]);
+            int max = Integer.parseInt(temp[1]);
+            String pass = str.get(2).trim();
+
+            int count = (int) pass.chars().filter(c -> c == value).count();
+
+            if (count >= min && count <= max) {
+                valid++;
+            }
+        }
+        System.out.println(valid);
+        */
+
+        // --------- VERSION 2 ----------
+        // Part 1 Counter
         int valid = 0;
+        // Part 2 Counter
         int valid2 = 0;
         for (String s : input) {
             passwordManager pm = new passwordManager(s);
-            System.out.println(pm);
             int count = (int) pm.pass.chars().filter(c -> c == pm.value).count();
 
             if (count >= pm.min && count <= pm.max) {
                 valid++;
             }
-            if ((pm.pass.charAt(pm.min-1) == pm.value || pm.pass.charAt(pm.max-1) == pm.value) &&
-                    !(pm.pass.charAt(pm.min-1) == pm.value && pm.pass.charAt(pm.max-1) == pm.value)) {
-                valid2++;
 
-            }
+            char charAtMin = pm.pass.charAt(pm.min-1);
+            char charAtMax = pm.pass.charAt(pm.max-1);
+            boolean isValid = (charAtMin == pm.value)^(charAtMax == pm.value);
+
+            if(isValid) valid2++;
+
         }
         System.out.println(valid);
         System.out.println(valid2);
